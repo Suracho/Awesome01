4f568f322a90d300edc7fa1a957552b0620b092d
Suraj <suracho@outlook.com>
2023-12-01 19:32:39 +1100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView, Button} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Hello World </Text>
+        <Button title="Hejehe" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ AppPro.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  useColorScheme,
+  Appearance
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  Appearance.setColorScheme('dark');
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello World
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+export default AppPro;
